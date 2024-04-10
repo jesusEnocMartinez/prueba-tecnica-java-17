2d2695cfb071c517397fc08afeba84583ef05ee4
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2024-04-10 12:37:41 -0600

    jesusEnoc/10/04/2024/Se agrego documentación tecnica

---
+++ documentacion/INSERT_CHARACTER.sql
@@ -0,0 +1,5 @@
+INSERT INTO CHARACTER_MODEL (name, age, gender, last_Name, ability, suit, attribute1, attribute2, attribute3, attribute4, attribute5, attribute6)
+VALUES
+  ('Superman', 35, 'Male', 'Kent', 'Flight, Super Strength', 'Blue and Red Suit', 'x-ray vision', 'heat vision', 'frost breath', 'super speed', 'invulnerability', 'super hearing'),
+  ('Batman', 40, 'Male', 'Wayne', 'Detective skills, Martial arts', 'Batsuit', 'rich', 'genius intellect', 'billionaire', 'night vision', 'stealth', 'bat-gadgets'),
+  ('Wonder Woman', 50, 'Female', 'Prince', 'Super strength, Flight', 'Amazonian Armor', 'immortality', 'combat skills', 'lasso of truth', 'divine wisdom', 'sword and shield', 'golden tiara');
